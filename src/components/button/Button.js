087bff3fdc98c9bc1701e6bb42709021e08c41d7
황중섭 <jshwang9709@gmail.com>
2024-06-05 22:45:12 +0900

    feat: 입차 버튼 클릭 시 api 호출

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import './Button.css';
 
 const EntranceOrExit = () => {
+    const handleClick = () => {
+        axios.get('http://localhost:8080/api/car/entrance', {
+            withCredentials: true
+        })
+            .then(response => console.log(response.data))
+            .catch(error => console.error('Error:', error));
+    };
+
     return (
-        <button className="park-button">입 차</button>
+        <button className="park-button" onClick={handleClick}>입 차</button>
     )
 }
 
