1acacecec867fb6d47eee7bd7f89343289115c51
황중섭 <jshwang9709@gmail.com>
2024-06-06 21:08:40 +0900

    feat: 입출차 컴포넌트 추가

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,13 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import './Button.css';
 
 const EntranceOrExit = () => {
     const [sse, setSse] = useState(null);
+    const [buttonText, setButtonText] = useState('입차'); //버튼의 텍스트 관리
+    const [buttonDisabled, setButtonDisabled] = useState(false); // 버튼의 활성화 상태 관리
+    const buttonTextRef = useRef(buttonText); // buttonText 상태의 최신 값을 참조하는 ref
 
     const handleClick = () => {
         if (!sse) {
-            const eventSource = new EventSource('http://localhost:8080/api/car/entrance');
+            let eventSource;
+            if (buttonText === '입차') {
+                eventSource = new EventSource('http://localhost:8080/api/car/entrance');
+                setButtonText('입차중');
+            } else {
+                eventSource = new EventSource('http://localhost:8080/api/car/exit');
+                setButtonText('출차중');
+            }
+
             setSse(eventSource);
+            setButtonDisabled(true);
 
             eventSource.onopen = (event) => {
                 console.log("Connection to server opened.");
@@ -27,10 +39,14 @@ const EntranceOrExit = () => {
 
                 eventSource.close();
                 setSse(null);
+                setButtonText(buttonTextRef.current === '입차중' ? '출차' : '입차');
+                setButtonDisabled(false);
             };
         } else {
             sse.close();
             setSse(null);
+            setButtonText(buttonTextRef.current === '입차중' ? '출차' : '입차');
+            setButtonDisabled(false);
         }
     };
 
@@ -42,8 +58,14 @@ const EntranceOrExit = () => {
         };
     }, [sse]);
 
+    useEffect(() => {
+        buttonTextRef.current = buttonText; // buttonText 상태가 변경될 때마다 ref를 업데이트
+    }, [buttonText]);
+
     return (
-        <button className="park-button" onClick={handleClick}>입 차</button>
+        <button className="park-button" onClick={handleClick} disabled={buttonDisabled}>
+            {buttonText}
+        </button>
     )
 }
 
