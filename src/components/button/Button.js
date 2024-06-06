a82ad1f29d10ae1ac63ab1b7df932d1c0826d0a9
황중섭 <jshwang9709@gmail.com>
2024-06-06 18:51:04 +0900

    feat: sse 통신 구축

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './Button.css';
 
 const EntranceOrExit = () => {
+    const [sse, setSse] = useState(null);
+
     const handleClick = () => {
-        axios.get('http://localhost:8080/api/car/entrance', {
-            withCredentials: true
-        })
-            .then(response => console.log(response.data))
-            .catch(error => console.error('Error:', error));
+        if (!sse) {
+            const eventSource = new EventSource('http://localhost:8080/api/car/entrance');
+            setSse(eventSource);
+
+            eventSource.onopen = (event) => {
+                console.log("Connection to server opened.");
+            }
+
+            eventSource.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                console.log(data);
+            };
+
+            eventSource.onerror = (event) => {
+                if (event.eventPhase === EventSource.CLOSED) {
+                    console.log("Connection to server closed successfully");
+                } else {
+                    console.error("Error occurred: ", event);
+                }
+
+                eventSource.close();
+                setSse(null);
+            };
+        } else {
+            sse.close();
+            setSse(null);
+        }
     };
+
+    useEffect(() => {
+        return () => {
+            if (sse) {
+                sse.close();
+            }
+        };
+    }, [sse]);
 
     return (
         <button className="park-button" onClick={handleClick}>입 차</button>
