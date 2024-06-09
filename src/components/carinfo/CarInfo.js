2471bdcf1961ffcefd08e3b784225b179cb9cc4e
황중섭 <jshwang9709@gmail.com>
2024-06-09 15:01:29 +0900

    style: 차량 정보 관련 스타일 변경

--- src/components/carinfo/CarInfo.js
+++ src/components/carinfo/CarInfo.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import './CarInfo.css';
 import carImage from '../../img/tesla-model-y.png'; // Add your car image to the project
+import batteryImage from '../../img/battery.png';
 
 const CarInfo = () => {
     return (
-        <div className="car-info">
-            <img src={carImage} alt="Tesla Model Y" className="car-image" />
-            <div className="info">
-                <div className="info-item">
-                    <span>모델명: </span> <span>Tesla Model Y</span>
-                </div>
-                <div className="info-item">
-                    <span>차 종: </span> <span>중형 SUV</span>
-                </div>
-                <div className="info-item">
-                    <span>배터리: </span> <span>83%</span>
-                </div>
-                <div className="info-item">
-                    <span>현재 위치: </span> <span>세종대학교 대양 AI 센터 지하주차장</span>
+        <div>
+            <div className="car-info">
+                <img src={carImage} alt="Tesla Model Y" className="car-image"/>
+                <div className="info">
+                    <div className="info-item">
+                        <span className="item-property">모델명</span>
+                        <span>Tesla Model Y</span>
+                    </div>
+                    <div className="info-item">
+                        <span className="item-property">차 종</span>
+                        <span>중형 SUV</span>
+                    </div>
+                    <div className="info-item">
+                        <span className="item-property">배터리</span>
+                        <img src={batteryImage} alt="Battery" className="battery-image"/>
+                        <span>83%</span>
+                    </div>
                 </div>
             </div>
+            <div className="location-item">
+                <span className="item-property">현재 위치</span> <span>세종대학교 대양 AI 센터 지하주차장</span>
+            </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default CarInfo;
